92a4a018079def9deb7c78927c03ea9cf0a4cc67
Kenta-Imamiya <kenta.crockers@gmail.com>
2023-09-27 23:10:21 +0900

    Task6-2 model追加

--- db/migrate/20230719135744_create_comments.rb
+++ db/migrate/20230719135744_create_comments.rb
@@ -2,8 +2,6 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.text :body
-      t.bigint :user_id
-      t.bigint :article_id
 
       t.timestamps
     end

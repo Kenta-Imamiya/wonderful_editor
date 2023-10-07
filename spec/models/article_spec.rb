ec00a2e7aac1fd3bd653938391bb82547f857c6f
kenta imamiya <>
2023-10-07 22:43:28 +0900

    Task.6-3 validationの実装中。article_spec.rbへ記述だがエラーに対応中。

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
-RSpec.describe Article do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Article, type: :model do
+  context "title を指定しているとき" do
+    it "投稿が作られる" do
+      article = Article.new(title: "foo")
+      expect(article.valid?).to eq true
+    end
+  end
+
+  context "title を指定していないとき" do
+    it "投稿作成に失敗する" do
+    end
+  end
 end

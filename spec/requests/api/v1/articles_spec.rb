48180ff624dc59ddf2d5825c0de911d128edbcae
kenta imamiya <>
2023-10-14 01:32:56 +0900

    Rubocopのエラーを修正した。

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -10,11 +10,13 @@ RSpec.describe "Api::V1::Articles" do
 
     it "記事の一覧が取得できる" do
       subject
-      res = JSON.parse(response.body)
+      res = response.parsed_body
+      # res = JSON.parse(response.body)
 
       expect(response).to have_http_status(:ok)
       expect(res.length).to eq 3
-      expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
+      expect(res.pluck("id")).to eq [article3.id, article1.id, article2.id]
+      # expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
       expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
       expect(res[0]["user"].keys).to eq ["id", "name", "email"]
     end

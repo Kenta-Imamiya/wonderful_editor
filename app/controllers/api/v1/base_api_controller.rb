7349ab6dee3e50ac10f37f6df432720c2d536508
kenta imamiya <>
2023-10-17 06:04:04 +0900

    Task.7-4 Rspec/requests/article_spec.rbのcontext "指定した id の記事が存在しない場合" を一部修正。 期待したエラーが発生していなかったため修正した。

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::BaseApiController < ApplicationController
+  # before_action :authenticate_user!
+
+  # session[:user_id] = 1
+
+  # def current_user
+  #   current_user ||= User.find(session[:user_id]) if session[:user_id]
+  #   render json: current_user, serializer: Api::V1::ArticleDetailSerializer
+  # end
 end

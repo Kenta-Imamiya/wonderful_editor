a8be72aa91e09587e4563ba8cd644b1ce815e8e0
kenta imamiya <>
2023-10-30 05:05:57 +0900

    10/29作業分を修正。

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,10 +1,9 @@
 module Api::V1
   class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-
     private
 
-    def sign_up_params
-      params.permit(:email, :password, :password_confirmation)
-    end
+      def sign_up_params
+        params.permit(:email, :password, :password_confirmation)
+      end
   end
 end

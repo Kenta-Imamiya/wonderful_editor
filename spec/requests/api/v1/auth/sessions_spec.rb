205f2e1d677153072cf661d494473647c1ef17ab
kenta imamiya <>
2023-11-19 13:23:28 +0900

    Task.9-4 ログアウトAPIの実装完了。

--- spec/requests/api/v1/auth/sessions_spec.rb
+++ spec/requests/api/v1/auth/sessions_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Auth::Sessions" do
-  fdescribe "POST /v1/auth/sign_in" do
+  describe "POST /v1/auth/sign_in" do
     subject { post(api_v1_user_session_path, params:) }
 
     context "登録済のユーザー情報を送信したとき" do
@@ -47,6 +47,32 @@ RSpec.describe "Api::V1::Auth::Sessions" do
         expect(header["client"]).to be_blank
         expect(header["uid"]).to be_blank
         expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  describe "DELETE /v1/suth/sign_out" do
+    subject { delete(destroy_api_v1_user_session_path, headers:) }
+
+    context "ログアウトに必要な情報を送信したとき" do
+      let(:user) { create(:user) }
+      let!(:headers) { user.create_new_auth_token }
+
+      it "ログアウトできる" do
+        expect { subject }.to change { user.reload.tokens }.from(be_present).to(be_blank)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "誤った情報を送信したとき" do
+      let(:user) { create(:user) }
+      let!(:headers) { { "access-token" => "", "token-type" => "", "client" => "", "expiry" => "", "uid" => "" } }
+
+      it "ログアウトできない" do
+        subject
+        expect(response).to have_http_status(:not_found)
+        res = response.parsed_body
+        expect(res["errors"]).to include "User was not found or was not logged in."
       end
     end
   end
